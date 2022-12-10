877a35c58d6f9b0b4729e634c1bcda144f5a5d0f
TeodorRaychev <teodor.raychev.TR@gmail.com>
2022-12-10 14:55:10 +0200

    header, footer, 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './core/home/home.component';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent,
+    data: {
+      title: 'Home'
+    }
+  },
+  {
+    path: 'not-found',
+    component: PageNotFoundComponent,
+    data: {
+      title: '404'
+    }
+  },
+  {
+    path: '**',
+    redirectTo: '/not-found',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    PageNotFoundComponent
+  ],
+})
+export class CoreModule { }
